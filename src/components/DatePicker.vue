d84ae5f1aa1f9c33cb7c6480247b44b784efc2d9
Milad <miladfathi021@gmail.com>
2021-04-30 19:32:23 +0430

    Create date picker component

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <span class="text-gray-700 text-md ml-1">{{ label }}</span>
+    <input
+      ref="datepicker"
+      type="date"
+      class="w-full h-10 px-2 text-gray-400 border rounded-lg cursor-pointer flex items-center bg-gray-50"
+      v-model="datepicker"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DatePicker",
+
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+
+  data() {
+    return {
+      showDatePicker: false,
+      datepicker: "",
+    };
+  },
+
+  watch: {
+    datepicker(newValue) {
+      return this.$emit("input", newValue);
+    },
+  },
+
+  methods: {
+    toggleDatePicker() {
+      this.showDatePicker = !this.showDatePicker;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
